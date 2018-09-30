73374bf3975e8bd3abc488e7ebfbece50693b6c8
STEFAN-ES <32038779+STEFAN-ES@users.noreply.github.com>
2018-09-30 19:37:23 +0300

    сохраняю перед добавлением скриптов

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+var btntel = document.getElementsByClassName('header-contacts__button');
+var closemodal = document.getElementsByClassName('popup-close');
+var popup = getElementsByClassName('popup');
+var overlay = getElementsByClassName('overlay');
+
+  function modal(){
+    
+  }  
