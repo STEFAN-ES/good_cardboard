0788e45edefeef21618d69ab09da777cecd356cd
STEFAN-ES <32038779+STEFAN-ES@users.noreply.github.com>
2018-10-01 11:05:25 +0300

    коммит с маской js-1

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
-var btntel = document.getElementsByClassName('header-contacts__button');
-var closemodal = document.getElementsByClassName('popup-close');
-var popup = getElementsByClassName('popup');
-var overlay = getElementsByClassName('overlay');
+var btntel = document.querySelector('.header-contacts__button');
+var closemodal = document.querySelector('.popup-close');
+var popup = document.querySelector('.popup');
+var overlay = document.querySelector('.overlay');
 
   function modal(){
-    
-  }  
+    overlay.style.display = "block"
+    console.log(11)
+  };
+  function modalclose(){
+    overlay.style.display = "none"
+  };
+  
+  btntel.addEventListener('click', modal);
+  closemodal.addEventListener('click', modalclose);
